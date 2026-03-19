3e65c875c7db2d71f7742ae75fef242ddc47f1de
samuel_thyz <samuel.theytaz@eduvaud.ch>
2026-03-19 12:07:06 +0100

    ajout de la route pour la page "home"

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import Login from '../pages/login.vue';
 import Register from '../pages/register.vue';
+import Home from '../pages/home.vue';
+import Rent from '../pages/rent.vue';
+
 
 const routes = [
-    { path: '/Login', component: Login },
-    { path: '/Register', component: Register }
+    {path: '/Login', component: Login},
+    {path: '/Register', component: Register},
+    {path: '/Home', component: Home},
+    {path: '/Rent', component: Rent}
 ];
 
 const router = createRouter({
